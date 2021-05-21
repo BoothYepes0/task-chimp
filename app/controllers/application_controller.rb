8075247e7d65ee88c2408a016154bf4599adae3f
yepes0 <yepes0@chicagobooth.edu>
2021-05-21 01:52:05 +0000

    after Jelani heroically saving the day

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action(:load_current_user )
   
-  # Uncomment this if you want to force user s to sign in before any other actions
-  # before_action(:force_user _sign_in)
   
-  def load_current_user 
-    the_id = session[:user _id]
-    @current_user  = User .where({ :id => the_id }).first
-  end
-  
-  def force_user _sign_in
-    if @current_user  == nil
-      redirect_to("/user _sign_in", { :notice => "You have to sign in first." })
-    end
-  end
-
 end
--- app/models/user .rb
+++
@@ -1,5 +0,0 @@
-class User  < ApplicationRecord
-  validates :email, :uniqueness => { :case_sensitive => false }
-  validates :email, :presence => true
-  has_secure_password
-end
