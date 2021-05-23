1c7567b700795c151378aa29319a1fc3b3c15a95
yepes0 <yepes0@chicagobooth.edu>
2021-05-23 12:44:12 +0000

    after migrating tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_015040) do
+ActiveRecord::Schema.define(version: 2021_05_23_124133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id"
+    t.boolean "completed"
+    t.text "task_name"
+    t.integer "category"
+    t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
