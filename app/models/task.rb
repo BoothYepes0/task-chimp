63b59a81918cf25382433f375cdb9d12aba47f53
yepes0 <yepes0@chicagobooth.edu>
2021-05-25 19:40:11 +0000

    after working with Jelani to fix a few issues with the enums in forms

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  enum({ :category => { :work => 0, :family => 1, :school => 2, :other => 3 } })
+  enum({ :priority => { :high => 0, :medium => 1, :low => 2 } })
+
 end
