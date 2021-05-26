3e17b9688c540cfcb7cc8027681760c6b8e2a7d9
yepes0 <yepes0@chicagobooth.edu>
2021-05-26 17:19:10 +0000

    before trying to fix up the view my tasks page

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
+  
+  # right below we are creating an association method
+  belongs_to :user
+
+  # here are the enums that we
   enum({ :category => { :work => 0, :family => 1, :school => 2, :other => 3 } })
   enum({ :priority => { :high => 0, :medium => 1, :low => 2 } })
 
