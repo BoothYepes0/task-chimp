f1af0781b74a32d2241d45171917b76c83e96432
yepes0 <yepes0@chicagobooth.edu>
2021-05-28 13:27:09 +0000

    after adding new fields in user sign up process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+     protected
+
+          def configure_permitted_parameters
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password)}
+
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :current_password)}
+          end
+  def after_sign_up_path_for(resource)
+    tasks_path
+  end
+
   def after_sign_in_path_for(resource)
     tasks_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :tasks
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+
 end
